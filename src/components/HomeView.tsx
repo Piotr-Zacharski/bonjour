e743407b44897b318553716eb81fad0e22ab4658
Piotr-Zacharski <zacharski.p.h@gmail.com>
2023-01-10 10:05:02 +0100

    Initial commit

---
+++ src/components/HomeView.tsx
@@ -0,0 +1,15 @@
+import { type NextPage } from "next";
+import { CgHello } from "react-icons/cg";
+
+const HomeView: NextPage = () => {
+  return (
+    <>
+      <main className="flex min-h-screen flex-row items-center justify-center  bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        <h1 className="text-9xl text-white">Bonjour</h1>
+        <CgHello className="h-28 w-32 text-white" />
+      </main>
+    </>
+  );
+};
+
+export default HomeView;
